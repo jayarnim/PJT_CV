d292ec2918018d16cbbdb0ad03f3dd63e185547b
jayarnim <116495744+jayarnim@users.noreply.github.com>
2025-05-29 15:16:33 +0900

    Update model.py

--- fight_detection/model.py
+++ fight_detection/model.py
@@ -5,8 +5,8 @@ import torch.nn as nn
 class FightDetection(nn.Module):
     def __init__(
         self, 
-        hidden: list=[400, 256, 128, 64],
-        dropout: float=0.2,
+        hidden: list=[2304, 1028, 512, 256, 128],
+        dropout: float=0.5,
     ):
         super(FightDetection, self).__init__()
         # attr dictionary for load
@@ -45,21 +45,30 @@ class FightDetection(nn.Module):
     def _feature_extractor_generator(self):
         self.pretrained_model = torch.hub.load(
             repo_or_dir='facebookresearch/pytorchvideo',
-            model='slowfast_r50',
+            model='slowfast_r101',
             pretrained=True,
         )
+        
         self.pretrained_model = self.pretrained_model.eval()
 
         self.feature_extractor = nn.Sequential(
-            *list(self.pretrained_model.blocks[:])
+            *list(self.pretrained_model.blocks[:-1])
         )
 
-        for param in self.feature_extractor[:5].parameters():
+        for param in self.feature_extractor.parameters():
             param.requires_grad = False
 
     def _classifier_generator(self):
+        layer_list = [
+            nn.AdaptiveAvgPool3d((1, 1, 1)),
+            nn.Flatten(),
+            nn.Dropout(p=self.dropout),
+        ]
+        
+        layer_list.extend(list(self._generate_layers(self.hidden)))
+        
         self.classifier = nn.Sequential(
-            *list(self._generate_layers(self.hidden)),
+            *layer_list,
             nn.Linear(self.hidden[-1], 1)
         )
 
@@ -73,6 +82,6 @@ class FightDetection(nn.Module):
             idx += 1
 
     def _assert_arg_error(self):
-        CONDITION = (self.hidden[0] == 400)
-        ERROR_MESSAGE = f"First MLP input dim must be 400, but got {self.hidden[0]}"
+        CONDITION = (self.hidden[0] == 2304)
+        ERROR_MESSAGE = f"First MLP input dim must be 2304, but got {self.hidden[0]}"
         assert CONDITION, ERROR_MESSAGE
